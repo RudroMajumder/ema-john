1f6394788e7382b153cc1693fe45c5e02f44f517
RudroMajumder <majumder.rudrofficial@gmail.com>
2021-02-26 21:36:50 +0600

    ema

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -18,7 +18,7 @@ const Product = (props) => {
                 <p> by {seller} </p>
                 <h4 style={{color:'black'}}> ${price} </h4>
                 <h4 style={{color:'black'}}> Only {stock} left in stock,order soon</h4>
-                <button className="main-btn" onClick={()=> props.handleClick(props.singleProduct)}><FontAwesomeIcon icon={faShoppingCart} />Add to cart</button>
+                <button className="main-btn" onClick={()=> props.handleClick(props.singleProduct)}><FontAwesomeIcon icon={faShoppingCart} /><big>  Add to cart</big></button>
             </div>
         </div>
     );
