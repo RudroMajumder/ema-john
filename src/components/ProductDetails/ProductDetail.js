28242e03a3f87e7ace0e9ab9a52de25393e9b01c
RudroMajumder <majumder.rudrofficial@gmail.com>
2021-04-10 12:05:47 +0600

    product details updated

--- src/components/ProductDetails/ProductDetail.js
+++ src/components/ProductDetails/ProductDetail.js
@@ -4,17 +4,19 @@ import Product from '../Product/Product';
 
 const ProductDetail = () => {
     const {productKey} = useParams();
-    const [product,setProduct] = useState([]);
-    useEffect(()=>{
-        fetch('https://evening-brook-43405.herokuapp.com/'+productKey)
-        .then( res => res.json())
-        .then( data => setProduct(data))
-    },[productKey])
+    const [product, setProduct] = useState({});
 
+    useEffect(() => {
+        fetch('https://evening-brook-43405.herokuapp.com/product/'+ productKey)
+        .then(res => res.json())
+        .then(data => setProduct(data));
+    }, [productKey])
+    
+    
     return (
         <div>
-            <Product product={product}  showAddToCart={false}>
-            </Product>
+            <h1>Your Product Details.</h1>
+            <Product showAddToCart={false} product={product}></Product>
         </div>
     );
 };
